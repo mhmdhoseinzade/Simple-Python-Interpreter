f5717683e9ea12e32a28db65b8c9b62d827a2d58
Mhmd <m.hoseinzadeh91@gamil.com>
2021-09-06 00:26:08 +0430

    feature: multiple digit //-/*/+

--- cal.py
+++ cal.py
@@ -2,7 +2,7 @@
 from typing import Text
 
 
-INTEGER,PLUS,EOF = 'INTEGER','PLUS','EOF'
+INTEGER,PLUS,MINUS,MULTI,DIV,EOF = 'INTEGER','PLUS','MINUS','MULTI','DIV','EOF'
 
 class Token:
     
@@ -17,30 +17,62 @@ class Token:
 class Interpreter:
 
     def __init__(self,text):
-        self.text = text
+        self.text = text.replace(" ","")
         self.pos = 0
+        self.current_token = None
+        self.current_char = self.text[self.pos]
 
     def error(self):
         raise Exception("Error parsing input")
 
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1 :
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+    
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+
     def get_next_token(self):
 
-        text = self.text
-        if self.pos > len(self.text) -1:
-            return Token(EOF,None)
+        while self.current_char is not None:
 
-        current_char = text[self.pos]
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
 
-        if current_char.isdigit():
-            self.pos += 1
-            return Token(INTEGER,int(current_char))
-        
-        if current_char == "+":
-            self.pos += 1
-            return Token(PLUS,current_char)
+            if self.current_char.isdigit():
+                return Token(INTEGER,self.integer())
+            
+            if self.current_char == "+":
+                self.advance()
+                return Token(PLUS,self.current_char)
 
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS,self.current_char)
 
-        self.error()
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV,self.current_char)
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULTI,self.current_char)
+
+            self.error()
+        return Token(EOF,None)
     
     def eat(self,token_type):
         if self.current_token.type == token_type:
@@ -50,16 +82,27 @@ class Interpreter:
 
     def expr(self):
         self.current_token = self.get_next_token()
+        
         left = self.current_token
+
         self.eat(INTEGER)
 
-        self.eat(PLUS)
+        op = self.current_token
+        self.eat(op.type)
+
 
         right = self.current_token
         self.eat(INTEGER)
 
+        if op.type == PLUS:
+            return left.value + right.value
+        elif op.type == MINUS:
+            return left.value - right.value
+        elif op.type == DIV:
+            return left.value / right.value
+        else:
+            return left.value * right.value
 
-        return left.value + right.value
 
 
 
